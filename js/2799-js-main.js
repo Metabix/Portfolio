21b2bdd1d6ed7d9c209588f4e8f898e9a29bbebe
metabix <mohit.palliyilsa@ufl.edu>
2024-08-25 11:07:06 -0700

    embw page changes

--- js/2799-js-main.js
+++ js/2799-js-main.js
@@ -68,6 +68,22 @@ $(function() {
     scale: 1,
     viewFactor: 0.3,
   });
+  
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
   sr.reveal('.background');
   sr.reveal('.skills');
